0a29f3dbd47d937dcc190775267ac9d8aad1b897
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-16 18:49:11 -0400

    add page header

---
+++ src/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+    export let search_email = '';
+</script>
+
+<div class="bg-light py-2 px-5">
+	<nav class="navbar navbar-expand-lg navbar-light ">
+		<div class="container-fluid">
+		  <a class="navbar-brand" href="/">Scam Email Finder </a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="end-lined collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav mb-2 mb-lg-0">
+				
+				<li class="nav-item">
+				  <a class="nav-link active" href="/">Home</a>
+				</li>
+  
+				<li class="nav-item">
+				  <a class="nav-link" href="#">Report</a>
+				</li>
+			   
+				<li class="nav-item d-flex">
+				  <a class="nav-link" href="#">FAQs</a>
+				</li>
+			  </ul>
+			</div>
+		</div>
+	  </nav>
+
+	  <div class="container-fluid">
+		<form class="d-flex">
+		  <input class="form-control me-2" type="search" placeholder="Search Email" aria-label="Search" bind:value={search_email}>
+		  <button class="btn btn-outline-success" type="submit">Search</button>
+		</form>
+	  </div>
+</div>
+
+<style>
+.end-lined {
+	flex-grow: 0;
+}
+</style>
