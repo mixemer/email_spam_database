8d34a634ea103afe812eb9764b0fdba97328bf94
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-17 06:41:19 -0400

    build

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/mixemer/email_spam_database', // Update to point to your repository  
+        repo: 'https://github.com/mixemer/email_spam_database.git', // Update to point to your repository  
         user: {
             name: 'mixemer', // update to use your name
             email: 'mehmetshin96@gmail.com' // Update to use your email
