f1e5f650c947b95d3deb6aa7ccb9c85d3be1d548
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-16 23:04:43 -0400

    add a toast when search field is empty

---
+++ src/components/Toast.svelte
@@ -0,0 +1,15 @@
+<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header p-3">
+        <i class="fa-solid fa-circle-exclamation px-2 text-danger"></i>
+        
+        <strong class="me-auto">Seach Field Empty</strong>
+        <!-- <small>11 mins ago</small> -->
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <!-- <div class="toast-body">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        Search field is empty
+      </div> -->
+    </div>
+  </div>
