f60941857307f83451c9a2da822c1cc35a98dc8b
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-17 06:14:24 -0400

    build

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,15 +1,15 @@
 <script>
-	// import Header from "./components/Header.svelte";
-	// import TableDataBase from "./components/TableDatabase.svelte";
+	import Header from "./components/Header.svelte";
+	import TableDataBase from "./components/TableDatabase.svelte";
 
-	// let search_email = '';
+	let search_email = '';
 </script>
 
 <main class="">
-	<h1>asda</h1>
-	<!-- <Header bind:search_email={search_email} />
+
+	<Header bind:search_email={search_email} />
 
 	<br>
 
-	<TableDataBase bind:search_email={search_email} /> -->
+	<TableDataBase bind:search_email={search_email} />
 </main>
