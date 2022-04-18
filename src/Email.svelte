8dd2c7bf41c866fcceab90d794b720cf7ec90d70
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-18 09:23:39 -0400

    find emailData using id param

--- src/Email.svelte
+++ src/Email.svelte
@@ -1,14 +1,16 @@
 <script>	
 	import { router_names } from "./globals"
+    import { data } from "./dummy_data"
 	import Header from "./components/Header.svelte";
 
     // This is id of the email in the dummydata
     export let id = ""
+    let emailData = data.find(d => d.id == id)
 </script>
 
 <div>
 	<Header current="{router_names.email}/{id}"/>
-    <h1>Email Page id: {id}</h1>
+    <h1>Email Page id: {id}, email: {emailData.email}</h1>
 </div>
 
 <style>
