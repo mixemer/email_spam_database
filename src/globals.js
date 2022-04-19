a79f39a0c543e41461a20669d54d9dd993c4d191
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-19 13:31:37 -0400

    add scam_types

--- src/globals.js
+++ src/globals.js
@@ -4,3 +4,55 @@ export const router_names = {
     FAQs: "faqs",
     email: "email"
 }
+
+/*
+Seached on these websites.
+https://www.atg.wa.gov/5-common-e-mail-scams
+https://www.securitymetrics.com/blog/top-10-types-phishing-emails
+*/
+export const scam_types = [
+    {
+        scam_name:  "The Urgent Offer", 
+        detail: "Look at this offer for refinancing"
+    },
+    {
+        scam_name:  "The 'Official Notice'", 
+        detail: "These scams attempt to fool consumers into believing they’ve received an e-mail that requires them to take some action. Often purporting to be from government agencies, these e-mails notify you of a problem. This example was sent in May, a time when people are more likely to believe an announcement is from the IRS.  Here you’re supposed to be relieved that the IRS is acknowledging they received your payment, and then be anxious that there is a problem, and click without thinking."
+    },
+    {
+        scam_name:  "The Lottery", 
+        detail: "Foreign lottery scams are rampant. If you did not enter a lottery, you did not win a lottery. If you did enter the lottery, you still are very unlikely to win, and you would not be notified via e-mail. This is a straightforward scam to get your information."
+    },
+    {
+        scam_name:  "The Survey", 
+        detail: "These scams rely on people’s desire to weigh in on issues and be heard on the issues of the day. In an election year one flavor is the voting survey, but any hot topic will do: global warming, attitudes towards war, the handling of the latest natural disaster, and so on."
+    },
+    {
+        scam_name:  "The Government Maneuver", 
+        detail: ""
+    },
+    {
+        scam_name:  "The Friend Tactic", 
+        detail: ""
+    },
+    {
+        scam_name:  "The Billing Problem", 
+        detail: ""
+    },
+    {
+        scam_name:  "The Expiration Date", 
+        detail: ""
+    },
+    {
+        scam_name:  "The Virus or Compromised Account Scare", 
+        detail: ""
+    },
+    {
+        scam_name:  "The Friendly Bank", 
+        detail: ""
+    },
+    {
+        scam_name:  "The Tax Communication", 
+        detail: ""
+    },
+]
