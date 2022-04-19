a2db3774c9b68301794c52af3ba1b5118618b512
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-19 14:01:53 -0400

    Update NotFound.svelte

--- src/NotFound.svelte
+++ src/NotFound.svelte
@@ -1,2 +1,33 @@
-<h1 class="text-center">Page Not Found</h1>
-<a class="nav-link text-center" href="/" >Go Home</a>
+<h1 class="text-center">Page Not Found  </h1>
+
+<div class="center">
+    <div style="width:100%;height:0;padding-bottom:84%;position:relative;">
+        <iframe title="404" src="https://giphy.com/embed/H54feNXf6i4eAQubud" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    </div>
+</div>
+
+<div>
+    <button class="btn btn-success btn-lg" >
+        <a href="/" >Go Home</a>
+    </button>
+</div>
+
+<style>
+.center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+button {
+    margin:0 auto;
+    display:block;
+}
+a {
+    color: white;
+    text-decoration: none;
+}
+a:hover{
+    text-decoration: none;
+    color: inherit;
+}
+</style>
