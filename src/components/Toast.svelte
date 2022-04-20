48fec976f9c786d44442f3341139506e8e6157f5
Mehmet Sahin <mehmetshin96@gmail.com>
2022-04-19 23:29:53 -0400

    Update Toast.svelte

--- src/components/Toast.svelte
+++ src/components/Toast.svelte
@@ -1,15 +1,24 @@
+<script>
+  export let title;
+  export let success = false;
+</script>
+
 <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="toast-header p-3">
+  <div id="{success ? 'liveToastSuccess' : 'liveToast'}" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header p-3">
+      {#if success}
+      <i class="fa-solid fa-circle-check px-2 text-success"></i>
+      {:else}
         <i class="fa-solid fa-circle-exclamation px-2 text-danger"></i>
-        
-        <strong class="me-auto">Seach Field Empty</strong>
-        <!-- <small>11 mins ago</small> -->
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <!-- <div class="toast-body">
-        <i class="fa-solid fa-circle-exclamation"></i>
-        Search field is empty
-      </div> -->
+      {/if}
+      
+      <strong class="me-auto">{title}</strong>
+      <!-- <small>11 mins ago</small> -->
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
+    <!-- <div class="toast-body">
+      <i class="fa-solid fa-circle-exclamation"></i>
+      Search field is empty
+    </div> -->
   </div>
+</div>
